45d4a770af7f120238016c0be093c11bfe1b4e07
temitope_iyanoye <toppytech@gmail.com>
2024-06-27 11:39:05 +0100

    added @NonNull to getCurrentAuditor method

--- src/main/java/com/fultil/config/AuditorAwareImpl.java
+++ src/main/java/com/fultil/config/AuditorAwareImpl.java
@@ -2,6 +2,7 @@ package com.fultil.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.lang.NonNull;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -11,6 +12,7 @@ import java.util.Optional;
 public class AuditorAwareImpl implements AuditorAware<String> {
 
     @Override
+    @NonNull
     public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || !authentication.isAuthenticated()) {
